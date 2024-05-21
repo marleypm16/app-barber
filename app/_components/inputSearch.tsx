5ad1a0d3d755a6cb69a88270e37aa67ad1c17f89
marleypm16 <yarlempm16@gmail.com>
2024-05-21 10:48:50 -0300

    feat : add function to search in component

--- app/_components/inputSearch.tsx
+++ app/_components/inputSearch.tsx
@@ -1,16 +1,33 @@
+"use client";
 import React from 'react';
 import {Input} from "@/app/_components/ui/input";
 import {Button} from "@/app/_components/ui/button";
 import {SearchIcon} from "lucide-react";
+import {useRouter} from "next/navigation";
 
 const InputSearch = () => {
+    const router = useRouter()
+    const [search,setSearch] = React.useState('')
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!search) {
+            return;
+        }
+
+        router.push(`/barbershop?search=${search}`);
+    }
     return (
-        <div className="flex gap-4 items-center">
-            <Input/>
-            <Button size='icon' className='bg-[#8163FF] px-2'>
+        <form onSubmit={handleSubmit} className="flex gap-3 items-center">
+            <Input placeholder='Buscar' className='rounded-2xl' onChange={handleChange}
+                   value={search}/>
+            <Button  size='icon' className='bg-[#8163FF] px-2' type='submit'>
                 <SearchIcon/>
             </Button>
-        </div>
+        </form>
     );
 };
 
