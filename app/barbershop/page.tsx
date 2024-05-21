6a23f7bb4995372b64993b58227735ac1612b332
marleypm16 <yarlempm16@gmail.com>
2024-05-21 10:49:59 -0300

    feat : add barbershops page

---
+++ app/barbershop/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, {useEffect} from 'react';
+import {notFound, useSearchParams} from "next/navigation";
+import {BarberShop} from "@prisma/client";
+import {searchForBarberShop} from "@/app/_lib/searchBarberShop";
+import Header from "@/app/_components/header";
+import BarberShopItem from "@/app/_components/barberShopItem";
+const BarberShopsPage = () => {
+    const searchParams = useSearchParams();
+    const searchFor = searchParams.get("search");
+    const [barberShops, setBarberShops] = React.useState<BarberShop[]>([]);
+    useEffect(() => {
+        const fetchBarberShops = async () => {
+            if (!searchFor) return;
+            const foundRestaurants = await searchForBarberShop(searchFor);
+            setBarberShops(foundRestaurants);
+        };
+
+        fetchBarberShops();
+    }, [searchFor]);
+
+    if (!searchFor) {
+        return notFound();
+    }
+    return (
+        <>
+            <Header />
+            <div className="px-5 py-6">
+                <h2 className="mb-6 text-lg font-semibold">Barbearias Encontradas</h2>
+                <div className="flex w-full flex-col gap-6">
+                    {barberShops.map((barberShop) => (
+                        <BarberShopItem
+                            key={barberShop.id}
+                            barberShop={barberShop}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default BarberShopsPage;
