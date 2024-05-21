0d870420b3971c144cdba6efbe87a87bc52bd4b6
marleypm16 <yarlempm16@gmail.com>
2024-05-21 10:44:10 -0300

    feat : Componentização

--- app/barbershop/[id]/_components/serviceDetails.tsx
+++ app/barbershop/[id]/_components/serviceDetails.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import { Services} from "@prisma/client";
-import Image from "next/image";
-import {Button} from "@/app/_components/ui/button";
+import {BarberShop, Services} from "@prisma/client";
+import ServiceList from './serviceList';
 interface ServiceDetailsProps {
     services : Services[]
+    barberShop : BarberShop
 }
-const ServiceDetails = ({services} : ServiceDetailsProps) => {
+const ServiceDetails = ({services,barberShop} : ServiceDetailsProps) => {
     return (
         <div>
             <ul>
-                {services.map((service) => (
-                    <li className='flex items-center justify-between mb-3' key={service.id}>
-                        <div className='flex gap-3'>
-                            <div>
-                                <Image className='rounded-full' src={service.imageUrl} alt={service.name} width={60} height={60} />
-                            </div>
-                            <div>
-                                <h2 className='mb-1'>{service.name}</h2>
-                                <p className='text-primary'>{Number(service.price).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
-                            </div>
-                        </div>
-                        <Button className=''>Agendar</Button>
-                    </li>
-                ))}
+                <ServiceList barberShop={barberShop} services={services}/>
             </ul>
         </div>
     );
