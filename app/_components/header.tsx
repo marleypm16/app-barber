384fad7efad09ab14a649371e4468e8c7759311a
marleypm16 <yarlempm16@gmail.com>
2024-05-21 10:48:22 -0300

    feat : add navigation

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Image from "next/image";
 import {MenuIcon} from "lucide-react";
 import {Button} from "@/app/_components/ui/button";
+import Link from "next/link";
 const Header = () => {
     return (
         <header className='flex justify-between items-center '>
-            <Image src='./logo.svg' alt='logo' height={70} width={70}/>
-            <Button size="icon" className="bg-transparent text-black">
+            <Link href='/'><Image src='/logo.svg' alt='logo' height={100} width={100}/></Link>
+            <Button size="icon" className="bg-transparent text-white">
                 <MenuIcon/>
             </Button>
         </header>
