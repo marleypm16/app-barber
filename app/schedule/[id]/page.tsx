5f0c07420d768d9edd2274e6104825cfac7b95f6
marleypm16 <yarlempm16@gmail.com>
2024-05-21 10:49:43 -0300

    feat : add schedule page

---
+++ app/schedule/[id]/page.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {db} from "@/app/_lib/prisma";
+import ScheduleDetails from "./_components/scheduleDetails";
+import {notFound} from "next/navigation";
+import Header from "@/app/_components/header";
+interface SchedulePageProps {
+    params : {
+        id : string
+    }
+
+}
+const SchedulePage = async ({params} : SchedulePageProps) => {
+    const schedule = await db.appointments.findUnique({
+        where:{
+            id: params.id
+        },
+        include:{
+            barberShop:{
+                select:{
+                    name:true,
+                    imageUrl:true,
+                    address:true
+                }
+            },
+            service:{
+                select:{
+                    name:true,
+                    price:true
+
+                }
+            }
+        }
+    });
+    if (!schedule) {
+        return notFound()
+    }
+    return (
+        <div className='p-5'>
+            <Header/>
+            <div className='mb-3'>
+                <h1>Informações da reserva</h1>
+            </div>
+            <div >
+                <ScheduleDetails schedule={schedule}/>
+            </div>
+        </div>
+    );
+};
+
+export default SchedulePage;
