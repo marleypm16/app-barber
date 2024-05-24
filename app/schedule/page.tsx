ff743b72609fe1fb342cfb879f718d5db4b997f1
marleypm16 <yarlempm16@gmail.com>
2024-05-24 11:06:45 -0300

    feat : add header and check to check list length

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {db} from "@/app/_lib/prisma";
 import SchedulingItem from "@/app/_components/schedulingItem";
+import Header from "@/app/_components/header";
 
 const Page = async () => {
     const appointments = await db.appointments.findMany({
@@ -19,12 +20,20 @@ const Page = async () => {
         }
     })
     return (
-        <div>
-            <h1>Agendamentos</h1>
-            {appointments.map((schedule) => (
-                <SchedulingItem key={schedule.id} scheduling={schedule}/>
-            ))}
-        </div>
+            <>
+                <div className='p-5'>
+                    <Header />
+                </div>
+                <div className="px-5 py-6">
+                    {appointments.length === 0 ? <h2 className="mb-6 text-lg font-semibold">Sem Agendamentos</h2> : <h2 className="mb-6 text-lg font-semibold">Agendamentos</h2>}
+                    <div className="flex w-full flex-col gap-6">
+                    {appointments.map((schedule) => (
+                            <SchedulingItem key={schedule.id} scheduling={schedule}/>
+                        ))}
+                    </div>
+                </div>
+            </>
+
     );
 };
 
