b5ea78acf81432b6ed4a0de719c091ad052e8d41
marleypm16 <yarlempm16@gmail.com>
2024-05-24 11:11:25 -0300

    feat : add star icon

--- app/_components/barberShopItem.tsx
+++ app/_components/barberShopItem.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BarberShop} from "@prisma/client";
 import {Card, CardContent} from "@/app/_components/ui/card";
-import {Badge, StarIcon} from "lucide-react";
 import {Button} from "@/app/_components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import {Badge} from "@/app/_components/ui/badge";
+import {StarIcon} from "lucide-react";
 interface BarberShopItemProps {
     barberShop : BarberShop
 }
@@ -14,12 +15,6 @@ const BarberShopItem = ({barberShop} : BarberShopItemProps) => {
             <Card className="min-w-full max-w-full rounded-2xl">
                 <CardContent className="px-1 py-0 pt-1">
                     <div className="w-full h-[159px] relative">
-                        <div className="absolute top-2 left-2 z-50">
-                            <Badge fontVariant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
-                                <StarIcon size={12} className="fill-primary text-primary" />
-                                <span className="text-xs">5,0</span>
-                            </Badge>
-                        </div>
                         <Image
                             alt={barberShop.name}
                             src={barberShop.imageUrl}
@@ -29,6 +24,10 @@ const BarberShopItem = ({barberShop} : BarberShopItemProps) => {
                             fill
                             className="rounded-2xl"
                         />
+                        <Badge className="absolute top-2 left-2" variant="secondary">
+                            <StarIcon size={16} className='fill-primary text-primary mr-1 '/>
+                            5,0
+                        </Badge>
                     </div>
 
                     <div className="px-2 pb-3">
