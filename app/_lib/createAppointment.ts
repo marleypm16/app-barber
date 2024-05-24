9e711b3744fbceb4f684f0e9d51efd2db3106a41
marleypm16 <yarlempm16@gmail.com>
2024-05-24 11:11:01 -0300

    feat : delete console log and if statemant

--- app/_lib/createAppointment.ts
+++ app/_lib/createAppointment.ts
@@ -1,11 +1,6 @@
 "use server"
 import {db} from "@/app/_lib/prisma";
 export const createAppointment = async (date: Date | undefined , serviceId: string , barberShopId: string ) => {
-    if (date! < new Date()) {
-        console.log("Data inválida")
-        return
-
-    }
     const appointment = await db.appointments.create({
         data: {
             date: date!,
@@ -13,5 +8,4 @@ export const createAppointment = async (date: Date | undefined , serviceId: stri
             barberShopId: barberShopId,
         },
     })
-    console.log(appointment)
 }
