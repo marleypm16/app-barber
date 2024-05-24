6bd35c3793457522d7cc8d71a18717ac8d50789a
marleypm16 <yarlempm16@gmail.com>
2024-05-24 11:00:19 -0300

    feat : add badge and check to check if the schedule is still in progress

--- app/_components/schedulingItem.tsx
+++ app/_components/schedulingItem.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Prisma } from "@prisma/client";
-import {Badge} from "lucide-react";
+import {Badge} from "@/app/_components/ui/badge";
 import {Card, CardContent} from "@/app/_components/ui/card";
-import { format } from 'date-fns';
+import {format, isFuture} from 'date-fns';
 import {ptBR} from "date-fns/locale";
 import {Avatar, AvatarFallback, AvatarImage} from "@/app/_components/ui/avatar";
 import Link from "next/link";
@@ -24,20 +24,19 @@ interface schedulingItemProps {
     }>;
 }
 const SchedulingItem = ({scheduling} : schedulingItemProps) => {
+    const isConfirmed = isFuture(scheduling.date);
     return (
         <Link className='min-w-full'  href={`/schedule/${scheduling.id}`}>
         <Card className="min-w-full">
             <CardContent className="py-0 flex px-0">
                 <div className="flex flex-col gap-2 py-5 flex-[3] pl-5">
-                    <Badge  className="w-fit">
-                       Confirmado
+                    <Badge variant={isConfirmed ? "default" : "secondary"}  className="w-fit">
+                        {isConfirmed ? "Confirmado" : "Finalizado"}
                     </Badge>
                     <h2 className="font-bold">{scheduling.service.name}</h2>
-
                     <div className="flex items-center gap-2">
                         <Avatar className="h-6 w-6">
                             <AvatarImage src={scheduling.barberShop.imageUrl} />
-
                             <AvatarFallback>A</AvatarFallback>
                         </Avatar>
 
